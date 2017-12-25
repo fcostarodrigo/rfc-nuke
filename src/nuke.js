76a7a3193572491de1b3dae880db4ffba341c988
Rodrigo Fernandes da Costa <fcostarodrigo@yahoo.com>
2017-12-25 10:40:50 -0200

    Refactor

--- src/nuke.js
+++ src/nuke.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
+const util = require("util");
 const walk = require("rfc-walk");
+
+const rmdir = util.promisify(fs.rmdir);
+const unlink = util.promisify(fs.unlink);
 
 const nuke = async root => {
   const files = (await walk({ root, includeFolders: true })).reverse();
+
+  const removeDir = file => error =>
+    error.code === "EISDIR" ? rmdir(file) : Promise.reject(error);
+
   for (const file of files) {
-    try {
-      fs.unlinkSync(file);
-    } catch (error) {
-      if (error.code === "EISDIR") {
-        fs.rmdirSync(file);
-      } else {
-        throw error;
-      }
-    }
+    await unlink(file).catch(removeDir(file));
   }
 };
 
--- src/nuke.unit.test.js
+++ src/nuke.unit.test.js
@@ -1,5 +1,27 @@
+const fs = require("fs");
+const walk = require("rfc-walk");
+const nuke = require("./nuke");
+
+jest.mock("fs");
+jest.mock("rfc-walk");
+
+const mockCallbacks = (caller, expectations) => {
+  for (const { result, args: expectedArgs } of expectations) {
+    caller.mockImplementationOnce((...args) => {
+      expect(args.slice(0, -1)).toEqual(expectedArgs);
+      args.pop()(...result);
+    });
+  }
+};
+
 describe("nuke", () => {
-  it("should behave...", () => {
-    expect(true).toBe(true);
+  beforeAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it("should delete a file", async () => {
+    walk.mockReturnValue(["a"]);
+    mockCallbacks(fs.unlink, [{ result: [null], args: ["a"] }]);
+    await nuke();
   });
 });
