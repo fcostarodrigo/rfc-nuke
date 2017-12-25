b2ad3d5bf660ef9d9652660e7dbd8726b0b5a052
Rodrigo Fernandes da Costa <fcostarodrigo@yahoo.com>
2017-12-25 10:49:41 -0200

    Add unit tests

--- src/nuke.unit.test.js
+++ src/nuke.unit.test.js
@@ -24,4 +24,45 @@ describe("nuke", () => {
     mockCallbacks(fs.unlink, [{ result: [null], args: ["a"] }]);
     await nuke();
   });
+
+  it("should delete a folder", async () => {
+    walk.mockReturnValue(["a"]);
+    mockCallbacks(fs.unlink, [{ result: [{ code: "EISDIR" }], args: ["a"] }]);
+    mockCallbacks(fs.rmdir, [{ result: [null], args: ["a"] }]);
+    await nuke();
+  });
+
+  it("should propagate unlink errors", async () => {
+    const error = new Error();
+    walk.mockReturnValue(["a"]);
+    mockCallbacks(fs.unlink, [{ result: [error], args: ["a"] }]);
+    await expect(nuke()).rejects.toThrow(error);
+  });
+
+  it("should propagate rmdir errors", async () => {
+    const error = new Error();
+    walk.mockReturnValue(["a"]);
+    mockCallbacks(fs.unlink, [{ result: [{ code: "EISDIR" }], args: ["a"] }]);
+    mockCallbacks(fs.rmdir, [{ result: [error], args: ["a"] }]);
+    await expect(nuke()).rejects.toThrow(error);
+  });
+
+  it("should delete files before their folders", async () => {
+    walk.mockReturnValue(["a", "a/b"]);
+    mockCallbacks(fs.unlink, [
+      { result: [null], args: ["a/b"] },
+      { result: [{ code: "EISDIR" }], args: ["a"] }
+    ]);
+    mockCallbacks(fs.rmdir, [{ result: [null], args: ["a"] }]);
+    await nuke();
+  });
+
+  it("should delete sibling files", async () => {
+    walk.mockReturnValue(["a", "b"]);
+    mockCallbacks(fs.unlink, [
+      { result: [null], args: ["b"] },
+      { result: [null], args: ["a"] }
+    ]);
+    await nuke();
+  });
 });
